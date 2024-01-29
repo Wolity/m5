e88201afa7491156818311b7b5f922f7e6bf36cd
Wolity <astapovsasa6@gmail.com>
2024-01-29 11:17:34 +0200

    new

--- GitPage/main.js
+++ GitPage/main.js
@@ -11,12 +11,48 @@ async function getCard() {
   decor.classList.add("none");
   load.classList.remove("none");
   card.classList.add("none");
-  console.log(searchValue.value)
+  console.log(searchValue.value);
   let response = await fetch(
     `https://api.github.com/users/${searchValue.value}`
   );
   if (response.ok) {
     dataCard = await response.json();
     console.log("Данные", dataCard);
-  } 
+    generateCard();
+  } else {
+    decor.classList.remove("none");
+  }
+  load.classList.add("none");
+  searchValue.value = "";
+}
+
+function generateCard() {
+  card.innerHTML = `      <img
+  src="${dataCard.avatar_url}"
+/>
+<h1>${dataCard.login}</h1>
+<p>${dataCard.bio}</p>
+<nav>
+  <div class="text-blue">
+    <i class="fas fa-marker"></i>
+    <span>${dataCard.location} </span>
+  </div>
+
+  <div class="text-red">
+    <i class="fas fa-thumbs-up"></i>
+    <span> ${dataCard.followers} </span>
+  </div>
+
+  <div class="text-yellow">
+    <i class="fas fa-star"></i>
+    <span> ${dataCard.following} </span>
+  </div>
+
+  <div class="text-green">
+    <i class="fas fa-bookmark"></i>
+    <span> ${dataCard.public_repos} </span>
+  </div>
+</nav>
+<a href="${dataCard.html_url}"> перейти </a>`;
+card.classList.remove("none")
 }
